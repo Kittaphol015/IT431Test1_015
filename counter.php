9e0842810ff6fbbb3562853290df3e4d8af8cb43
Kittaphol015 <kittaphol.s@rmutsvmail.com>
2025-01-16 16:35:40 +0700

    Update counter.php #1

--- counter.php
+++ counter.php
@@ -8,11 +8,11 @@
     while ($dbarr = mysqli_fetch_array($result)){
         $pgcount = $dbarr["countnum"];
     }
-    $pgcount = $pgcount + 1;
-    $pgcount = "00000" . $pgcount;
-    $pgcount = substr($pgcount, -6);
+    $pgcount = $pgcount + 1; //Kittaphol
+    $pgcount = "00000" . $pgcount; //Kittaphol
+    $pgcount = substr($pgcount, -6); //Kittaphol
     echo "$pgcount";
-
+    
     $sql = "Update counter set countnum = '$pgcount' where id = 1";
     $result = mysqli_query($link,$sql);
 ?>
